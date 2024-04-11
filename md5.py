d1abc7c6faf7fbc929b7f0e5f38e7b4b138ef228
marzooqy <marzooqy@users.noreply.github.com>
2024-04-11 07:58:56 +0300

    Delete md5.py

--- md5.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-import sys
-
-with open(sys.argv[1], 'rb') as file:
-    with open(sys.argv[2], 'rb') as file2:
-        print(hashlib.md5(file.read()).hexdigest())
-        print(hashlib.md5(file2.read()).hexdigest())
