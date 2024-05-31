a66dd8b8ec57879716b09a451334e066e8ce2b9c
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2024-05-31 13:39:46 +0300

    Compile with /O2

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-cl /EHsc /std:c++20 /openmp lz77.cpp
+cl /EHsc /std:c++20 /openmp /O2 lz77.cpp
 
 del lz77.obj
 
